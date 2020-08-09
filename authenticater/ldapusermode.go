4a24c9173d14e478e4ed397da3919210d36593d0
Marcus Soll <git@msoll.eu>
2020-08-09 21:18:16 +0200

    Added support for LDAP in user mode

---
+++ authenticater/ldapusermode.go
@@ -0,0 +1,120 @@
+// build +ldap
+
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 Marcus Soll
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package authenticater
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+
+	"github.com/Top-Ranger/pollgo/registry"
+	"github.com/go-ldap/ldap/v3"
+)
+
+func init() {
+	err := registry.RegisterAuthenticater(&LDAPUserMode{}, "LDAP-usermode")
+	if err != nil {
+		panic(err)
+	}
+}
+
+// LDAPUserMode is an authenticator for using LDAP in user mode.
+// It creates a new connection for every call to Authenticate and tries to bind the user.
+type LDAPUserMode struct {
+	Endpoint                      string
+	UseStartTLS                   bool
+	BindUserPattern               string
+	TimeLimit                     int
+	BaseDN                        string
+	LDAPUserFilter                string
+	InsecureSkipCertificateVerify bool
+}
+
+// LoadConfig loads the LDAP configuration as a JSON.
+func (l *LDAPUserMode) LoadConfig(b []byte) error {
+	err := json.Unmarshal(b, l)
+	if err != nil {
+		return err
+	}
+
+	// Test connection
+	conn, err := ldap.DialURL(l.Endpoint, ldap.DialWithTLSConfig(&tls.Config{InsecureSkipVerify: l.InsecureSkipCertificateVerify}))
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	if l.UseStartTLS {
+		err = conn.StartTLS(nil)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Authenticate verifies a user / password combination by binding it to the LDAP server.
+func (l *LDAPUserMode) Authenticate(user, password string) (bool, error) {
+	// Connect
+	conn, err := ldap.DialURL(l.Endpoint, ldap.DialWithTLSConfig(&tls.Config{InsecureSkipVerify: l.InsecureSkipCertificateVerify}))
+	if err != nil {
+		return false, err
+	}
+	defer conn.Close()
+
+	if l.UseStartTLS {
+		err = conn.StartTLS(nil)
+		if err != nil {
+			return false, err
+		}
+	}
+
+	err = conn.Bind(fmt.Sprintf(l.BindUserPattern, user), password)
+	if err != nil {
+		return false, err
+	}
+
+	// Get User
+	searchRequest := ldap.NewSearchRequest(
+		l.BaseDN,
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, l.TimeLimit, false,
+		fmt.Sprintf(l.LDAPUserFilter, user),
+		[]string{"dn"},
+		nil,
+	)
+
+	searchResults, err := conn.Search(searchRequest)
+	if err != nil {
+		return false, err
+	}
+
+	if len(searchResults.Entries) != 1 {
+		return false, nil
+	}
+
+	dn := searchResults.Entries[0].DN
+
+	// Bind to user
+	err = conn.Bind(dn, password)
+	if err != nil {
+		return false, nil
+	}
+
+	return true, nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Top-Ranger/pollgo
 go 1.14
 
 require (
+	github.com/go-ldap/ldap/v3 v3.2.3
 	github.com/go-playground/colors v1.2.0
 	github.com/microcosm-cc/bluemonday v1.0.3
 	github.com/yuin/goldmark v1.2.0
